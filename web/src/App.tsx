dfa597b8b4aef9568034279ade6bced581943e7f
2sangjin <94418901+2sangjin@users.noreply.github.com>
2022-09-22 23:26:54 +0900

    infinite loading, randomlist

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 // import logo from "./logo.svg";
 import "./App.css";
-import { Main } from "./components/Main/Main";
+import { MainPage } from "./components/MainPage/MainPage";
 // import { InfiniteScroll } from "./components/Main/Infinite_scroll";
 
 function App() {
   return (
-    <Main></Main>
+    <MainPage></MainPage>
     // <div className="App">
     //   <header className="App-header">
     //     <img src={logo} className="App-logo" alt="logo" />
