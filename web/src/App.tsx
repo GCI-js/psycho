599addcb0f32d21843f7f65a396fd617d5f6f86d
2sangjin <50646186+sangjinn@users.noreply.github.com>
2022-09-22 23:33:29 +0900

    app init

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,28 +1,28 @@
 import React from "react";
-// import logo from "./logo.svg";
+import logo from "./logo.svg";
 import "./App.css";
-import { MainPage } from "./components/MainPage/MainPage";
-// import { InfiniteScroll } from "./components/Main/Infinite_scroll";
+// import MBTIHistogram from "./components/profile/MBTIHistorgram";
+import Profile from "./components/profile/Profile";
 
 function App() {
   return (
-    <MainPage></MainPage>
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.tsx</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
+    <div className="App">
+      <Profile />
+      {/* <header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.tsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React
+				</a>
+			</header> */}
+    </div>
   );
 }
 
