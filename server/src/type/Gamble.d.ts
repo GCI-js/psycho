4bf2488dfcc561f47a38472fd333a2659ff92168
jeff.park <jinopark1119@gmail.com>
2022-09-29 21:30:17 +0900

    Gamble type modified, get api for associated hashtags added

--- server/src/type/Gamble.d.ts
+++ server/src/type/Gamble.d.ts
@@ -3,20 +3,19 @@ import { Document } from "mongoose";
 declare interface Gamble{
   "gamble_id": string
   "survey_id": string
+  "date": number
   "due": number
   "title": string
   "contents": {
     "main": string
     "options": Option[]
   }
-  "state": [
-    {
-      "index": number
-      "name": string
-      "user_cnt": number
-      "balance": number
-      "dividend": number //배당률
-    }
-  ],
+  "state":{
+    "index": number
+    "name": string
+    "user_cnt": number
+    "balance": number
+    "dividend": number //배당률
+  }[],
   "result": number // one of index. answer. should not be transferred until due.
 }
