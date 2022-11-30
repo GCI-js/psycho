5b7b5d013fc935fb6c836a07bdce001289f9271e
js_lee <jongsuklee45@gmail.com>
2022-12-01 02:09:32 +0900

    feat: edit profile css

--- web/src/jongseok/components/Profile/Elements/Gamble.tsx
+++ web/src/jongseok/components/Profile/Elements/Gamble.tsx
@@ -10,7 +10,7 @@ const Gamble = () => {
   };
   return (
     <div className="gambleContainer">
-      <text className="title">나의 도박 기록</text>
+      <text className="gambleTitle">나의 도박 기록</text>
       <text className="gambleContent">{`지난 ${dummyGambleData.date}일 동안 ${dummyGambleData.winCount}번 이기셨고, ${dummyGambleData.loseCount}번 패배하셨어요! 총 승률은 ${dummyGambleData.winRate}% 에요! 지금까지 ${dummyGambleData.point}포인트를 사용하셨어요!`}</text>
     </div>
   );
