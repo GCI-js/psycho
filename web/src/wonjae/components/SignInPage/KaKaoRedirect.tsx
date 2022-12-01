0b1821755125c34534814ac3c7b7ca3a07623e47
swjnice@github.com <swjnice@gmail.com>
2022-12-01 18:52:58 +0900

    main pull done

--- web/src/wonjae/components/SignInPage/KaKaoRedirect.tsx
+++ web/src/wonjae/components/SignInPage/KaKaoRedirect.tsx
@@ -2,15 +2,6 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import kakaoUser from "../../../../../common/type/KaKaoAuth";
 
-// kakao auth return value interface
-// interface kakaoUser {
-//   access_token: String;
-//   expires_in: Number;
-//   refresh_token: String;
-//   refresh_token_expires_in: Number;
-//   token_type: String;
-// }
-
 // backend return value interface
 interface serverProps {}
 
