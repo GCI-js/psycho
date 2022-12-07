7b0b28401d920b34ae2d65a533235ad1541c794b
jongseokleedev <jongsuklee45@gmail.com>
2022-12-08 00:37:59 +0900

    add custom dropdown

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 
 // import BettingGraph from "./betting_graph/App";
 // import ProfileStats from "./profile_stats/App";
-// import Sangjin from "./sangjin/App";
+import Sangjin from "./sangjin/App";
 // import Wonjae from "./wonjae/App";
 import Seoha from "./seoha/App";
 import Jongseok from "./jongseok/App";
@@ -14,7 +14,7 @@ import "./index.scss";
 // createRoot(document.getElementById('app')).render(<BettingGraph/>);
 // createRoot(document.getElementById('app')).render(<ProfileStats/>);
 // createRoot(document.getElementById('app')).render(<Wonjae/>);
-// createRoot(document.getElementById("app")).render(<Jongseok />);
+// createRoot(document.getElementById("app")).render(<Sangjin />);
 // createRoot(document.getElementById("app")).render(<Seoha />);
 // createRoot(document.getElementById("app")).render(<Jongseok />);
 createRoot(document.getElementById("app")).render(<Seoha />);
--- web/src/seoha/App.tsx
+++ web/src/seoha/App.tsx
@@ -4,20 +4,20 @@ import Lamb from "./component/Lamb";
 import LoginModal from "./component/LoginModal";
 import MainPage from "./component/MainPage";
 
-import './App.scss';
+import "./App.scss";
 import shepherd from "./service/shepherd";
 
-
 export default function App() {
-    console.log("<App/>");
+  console.log("<App/>");
 
-    React.useEffect(() =>
-        window.addEventListener("popstate", shepherd.whip), []);
+  React.useEffect(() => window.addEventListener("popstate", shepherd.whip), []);
 
-    return <div className="app-j238dndx8w4hweh">
-        <Lamb className="router" data-lamb="lamb">
-            <LoginModal data-pose="login"/>
-            <MainPage data-pose="main"/>
-        </Lamb>
+  return (
+    <div className="app-j238dndx8w4hweh">
+      <Lamb className="router" data-lamb="lamb">
+        <LoginModal data-pose="login" />
+        <MainPage data-pose="main" />
+      </Lamb>
     </div>
+  );
 }
