04753335ed1b9e6c8e2adbf1b68db4337e7fa0f5
giraffeJ <jinopark1119@gmail.com>
2022-12-15 18:39:16 +0900

    Gamble modules updating

--- server/src/database/controller/GambleController.ts
+++ server/src/database/controller/GambleController.ts
@@ -11,8 +11,42 @@ export const GambleController = {
     console.log(maxId);
     return maxId + 1;
   },
-  createGamble: async (newGamble: Gamble) => {
+  createGamble: async (req: Request, res: Response) => {
+    console.log("createGamble");
+    let gambleId: string = await (await GambleController.getNewId()).toString();
+    console.log(`New id: ${gambleId}`);
+    let newGamble: Gamble = {
+      gambleId: gambleId,
+      surveyId: req.body.surveyId,
+      openTime: req.body.openTime,
+      closeTime: req.body.closeTime,
+      title: req.body.title,
+      contents: {
+        main: req.body.contents.main,
+        options: req.body.contents.options,
+      },
+      betState: [
+        {
+          index: 0,
+          name: req.body.contents.options[0].name,
+          userCnt: 0,
+          balance: 0,
+          dividend: 0,
+        },
+        {
+          index: 1,
+          name: req.body.contents.options[1].name,
+          userCnt: 0,
+          balance: 0,
+          dividend: 0,
+        },
+      ],
+      result: req.body.result,
+      answerIndex: req.body.answerIndex,
+    }
     await GambleModel.create(newGamble);
+    let filter = {gambleId: gambleId};
+    res.status(200).json(await GambleModel.findOne(filter).lean<Gamble>());
   },
   findAll: async () => {
     let gambles = await GambleModel.find({}).lean<Gamble>();
