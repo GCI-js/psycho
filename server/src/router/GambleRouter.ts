718e45b3bc752fb679b644e6e637bf581774ef45
giraffeJ <jinopark1119@gmail.com>
2022-12-15 23:14:48 +0900

    gamble router&controller updated

--- server/src/router/GambleRouter.ts
+++ server/src/router/GambleRouter.ts
@@ -15,60 +15,6 @@ GambleRouter.get("/items/:id", GambleController.findById);
 GambleRouter.put("/bet", GambleController.bet)
 GambleRouter.put("/results/:gambleId/:userId", GambleController.reflectResult);
 
-GambleRouter.post("/", async (req: Request, res: Response) => {
-  let gambleId: string = ((await GambleController.getNewId()) + 1).toString();
-  console.log(gambleId);
-  let newGamble: Gamble = {
-    gambleId: gambleId,
-    surveyId: req.body.surveyId,
-    openTime: req.body.openTime,
-    closeTime: req.body.closeTime,
-    title: req.body.title,
-    contents: {
-      main: req.body.contents.main,
-      options: req.body.contents.options,
-    },
-    betState: [
-      {
-        index: 0,
-        name: req.body.contents.options[0].name,
-        userCnt: 0,
-        balance: 0,
-        dividend: 0,
-      },
-      {
-        index: 1,
-        name: req.body.contents.options[1].name,
-        userCnt: 0,
-        balance: 0,
-        dividend: 0,
-      },
-    ],
-    result: req.body.result,
-    answerIndex: req.body.answerIndex,
-  };
-  await GambleController.createGamble(newGamble);
-  res.status(200).json(await GambleController.findOne(gambleId));
-});
-
-GambleRouter.put("/items/:id", async (req: Request, res: Response) => {
-  await GambleController.findOneAndUpdate(req.params.id, req.body);
-  res.status(200).json(await GambleController.findOne(req.params.id));
-});
-
-GambleRouter.delete("/items/:id", async (req: Request, res: Response) => {
-  await GambleController.findOneAndDelete(req.params.id);
-  res.status(200).json(await GambleController.findAll());
-});
-
-GambleRouter.get("/items", async (req: Request, res: Response) => {
-  res.status(200).json(await GambleController.findAll());
-});
-
-GambleRouter.get("/items/:id", async (req: Request, res: Response) => {
-  res.status(200).json(await GambleController.findOne(req.params.id));
-});
-
 GambleRouter.put("/bet", async (req: Request, res: Response) => {
   let user: User = await UserController.findOne(req.body.userId);
   if (
