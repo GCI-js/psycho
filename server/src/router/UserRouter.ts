b3645faa45278d7c82122487ad0786e90e26b088
JeonYongHyun <dydgus9650@gmail.com>
2022-12-22 23:16:27 +0900

    Google Auth added

--- server/src/router/UserRouter.ts
+++ server/src/router/UserRouter.ts
@@ -7,3 +7,5 @@ UserRouter.get("/items", UserController.readUsers);
 UserRouter.put("/items/:userId", UserController.updateUser);
 UserRouter.delete("/items/:userId", UserController.deleteUser);
 UserRouter.get("/count", UserController.readUserCount);
+UserRouter.post("/auth/google", UserController.authGoogle);
+// UserRouter.post("/auth/google/refresh",UserController.authGoogle);
