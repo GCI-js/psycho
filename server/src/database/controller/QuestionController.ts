43b72878dc74101f1a3b99bf921df5ecfc733f7e
jeff.park <jinopark1119@gmail.com>
2022-12-22 20:40:42 +0900

    Question CRUD api created

--- server/src/database/controller/QuestionController.ts
+++ server/src/database/controller/QuestionController.ts
@@ -1,19 +1,41 @@
 import { QuestionModel } from "../model/QuestionModel";
 import { Question } from "../../../../common/type/Question";
+import { Request, Response } from "express";
+import { GetNewId } from "../../component/Util";
 
 export const QuestionController = {
-  getQuestionNum: async () => {
-    let cnt = await QuestionModel.count({});
-    return cnt;
-  },
-  createQuestion: async (newQuestion: Question) => {
+  createQuestion: async (req: Request, res: Response) => {
+    console.log("createQuestion");
+    let newQuestionId: string = await GetNewId(QuestionModel, "questionId");
+    let newQuestion: Question = {
+      questionId: newQuestionId,
+      image: req.body.image,
+      title: req.body.title,
+      type: req.body.type,
+      date: req.body.date,
+      contents: req.body.contents,
+      mbtiChange: req.body.mbtiChange,
+    };
     await QuestionModel.create(newQuestion);
+    let filter = { questionId: newQuestionId };
+    res.status(200).json(await QuestionModel.findOne(filter).lean<Question>());
   },
-  findAllQuestions: async () => {
-    return QuestionModel.find({});
+  readQuestions: async (req: Request, res: Response) => {
+    console.log("readQuestions");
+    res
+      .status(200)
+      .json(await QuestionModel.find(req.query).lean<Question[]>());
   },
-  findOne: async (questionId: string) => {
-    let filter = { questionId: questionId };
-    return await QuestionModel.findOne(filter).lean<Question>();
+  updateQuestion: async (req: Request, res: Response) => {
+    console.log("updateQuestion");
+    let filter = { questionId: req.params.questionId };
+    await QuestionModel.findOneAndUpdate(filter, req.body);
+    res.status(200).json(await QuestionModel.findOne(filter).lean<Question>());
+  },
+  deleteQuestion: async (req: Request, res: Response) => {
+    console.log("deleteQuestion");
+    let filter = { questionId: req.params.questionId };
+    await QuestionModel.findOneAndDelete(filter);
+    res.status(200).json({});
   },
 };
