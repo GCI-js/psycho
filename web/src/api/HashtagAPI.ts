e8b339fbe349011972f733ae247f5cc794483db8
jeff.park <jinopark1119@gmail.com>
2023-01-12 17:21:03 +0900

    HashtagAPI sample

--- web/src/api/HashtagAPI.ts
+++ web/src/api/HashtagAPI.ts
@@ -1,3 +1,19 @@
+const HEADERS = {
+  "Content-Type": "application/json",
+  Accept: "application/json",
+};
+
 export const HashtagAPI = {
-  createHashtag: async (name: string) => {},
+  createHashtag: async (name: string) => {
+    let body = new FormData();
+    body.append("name", name);
+    let res = await fetch(process.env.HTTP_SERVER_URL + "/hashtag/", {
+      method: "POST",
+      headers: HEADERS,
+      body: body,
+    }).then((res) => {
+      return res.json();
+    });
+    return res;
+  },
 };
