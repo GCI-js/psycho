356e25b2436ba2cdcddba6342e64259dbba18ebd
swjnice@github.com <swjnice@gmail.com>
2023-02-09 22:46:45 +0900

    css fixed

--- web/src/component/TermsInUsePage/index.tsx
+++ web/src/component/TermsInUsePage/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import shepherd from "../../service/shepherd";
 
 import idiotproof from "../../service/idiotproof";
@@ -16,35 +17,52 @@ export const TermsInUsePage = (properties: Props) => {
   const cl = [styles.index, properties.className].join(" ");
   console.log(styles);
 
+  // checkbox state
+  const [isAgreed, setIsAgreed] = useState(false);
+  const checkHandler = () => {
+    setIsAgreed(!isAgreed);
+  };
+
+  // navigation
   const handleRegisterDone = () => {
     localStorage.setItem("isOldUser", "true");
     properties.setNavVisible(true);
     shepherd.whip("test", "NewsletterPage");
   };
-  return (
-    <div className="Page">
-      <div id={id} className={cl}>
-        <div className="page_wrapper">
-          <div className="LargeTitle">이용약관</div>
 
-          <div className="term_box">
+  return (
+    <div id={id} className={cl}>
+      <div className="large_title">이용약관</div>
+
+      <div className="term_and_agree">
+        <div className="term_box">
+          <div className="terms_wrapper">
             <div className="terms">{terms}</div>
           </div>
+        </div>
 
+        <div className="agree_box">
           <div className="checkbox_wrapper">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              checked={isAgreed}
+              onChange={() => checkHandler()}
+            />
             <div className="text">
               본인은 19세 이상이며 이용약관에 동의합니다.
             </div>
           </div>
-
-          <MainButton
-            text="계속"
-            onClick={() => {
-              handleRegisterDone();
-            }}
-          />
         </div>
+      </div>
+
+      <div className="main_btn_wrapper">
+        <MainButton
+          text="계속"
+          isAgreed={isAgreed}
+          onClick={() => {
+            if (isAgreed) handleRegisterDone();
+          }}
+        />
       </div>
     </div>
   );
