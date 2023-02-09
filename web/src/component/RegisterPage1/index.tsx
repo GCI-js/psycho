e7d3906962f058fb582c1af93d8c493671ac0643
jeff.park <jinopark1119@gmail.com>
2023-02-09 22:32:16 +0900

    RegisterPage1 frame done. functional things left

--- web/src/component/RegisterPage1/index.tsx
+++ web/src/component/RegisterPage1/index.tsx
@@ -6,11 +6,17 @@ import styles from "./index.module.scss";
 import { MBTISelectBox } from "../MBTISelectBox";
 import { BloodTypeSelectBox } from "../BloodTypeSelectBox";
 import ArrowLeft from "../../img/Arrow_left.png";
+import NicknameFlag from "../../img/nicknameFlag.png";
 
-const RegisterPage1 = (properties: Properties) => {
+interface Props extends Properties {
+  setNavVisible: Function;
+}
+
+const RegisterPage1 = (properties: Props) => {
   const id = [`_${idiotproof.trace(RegisterPage1)}`, properties.id].join();
   const cl = [styles.index, properties.className].join(" ");
   const dummyUserName = "아크릴오므라이스";
+  properties.setNavVisible(false);
   /*
 [2022.01.12 jongseok lee] 
 MBTI 버튼 클릭했을시 이펙트 출력하는 부분 코드가 매우 더러워서 버리는게 나을 거 같다는 생각도 들음. 과감히 지워도 됨
@@ -23,22 +29,39 @@ MBTI 버튼 클릭했을시 이펙트 출력하는 부분 코드가 매우 더�
       <img className="back-button" src={ArrowLeft} onClick={handleBackButton} />
       <div className="large-title">회원가입</div>
       <div className="medium-title">
-        <p className="border">Psycho</p>에 등록된 계정이 없어요! 회원가입을
+        <div className="border">Psycho</div>에 등록된 계정이 없어요! 회원가입을
         하시려면 아래 내용을 채워주세요.
       </div>
       <div className="nickname-area">
-        <div className="small-title">닉네임</div>
-        <div>
-          <input type="text" placeholder={`@${dummyUserName}`} />
-          <button>랜덤 생성</button>
+        <div className="small-grey-title">
+          <div className="border">닉네임</div>
+        </div>
+        <div className="nickname-generator">
+          <img className="nickname-icon" src={NicknameFlag} />
+          <input
+            className="nickname-input"
+            type="text"
+            placeholder={`@${dummyUserName}`}
+          />
+          <button className="nickname-gen-button">랜덤 생성</button>
         </div>
       </div>
       <div className="mbti-area">
-        <div className="userDetailTitle">MBTI를 선택해주세요</div>
+        <div className="text">
+          <div className="small-grey-title">
+            <div className="border">MBTI</div>
+            {"를 선택해주세요  "}
+          </div>
+          <div className="tiny-grey-title">
+            (모르신다면 선택하지 않으셔도 괜찮아요!)
+          </div>
+        </div>
         <MBTISelectBox />
       </div>
       <div className="blood-type-area">
-        <div className="userDetailTitle">혈액형을 선택해주세요</div>
+        <div className="small-grey-title">
+          <div className="border">혈액형</div>을 선택해주세요
+        </div>
         <BloodTypeSelectBox />
       </div>
       <button
