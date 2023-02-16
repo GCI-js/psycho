e612a0b094e0ce9bb3bcaf8144a49adacc5aea26
jeff.park <jinopark1119@gmail.com>
2023-02-16 22:17:41 +0900

    MBTI value and state convert functions added

---
+++ web/src/service/convertMBTI.ts
@@ -0,0 +1,31 @@
+import { Mbti } from "../@types/common";
+
+interface MBTIState {
+  MBTI: string;
+  state: boolean;
+}
+
+export const MBTIValueToState = (mbti: Mbti) => {
+  let mbtiState = [
+    { MBTI: "E", state: mbti.EI < 50 ? true : false },
+    { MBTI: "N", state: mbti.NS < 50 ? true : false },
+    { MBTI: "F", state: mbti.FT < 50 ? true : false },
+    { MBTI: "J", state: mbti.JP < 50 ? true : false },
+    { MBTI: "I", state: mbti.EI > 50 ? true : false },
+    { MBTI: "S", state: mbti.NS > 50 ? true : false },
+    { MBTI: "T", state: mbti.FT > 50 ? true : false },
+    { MBTI: "P", state: mbti.JP > 50 ? true : false },
+  ];
+  return mbtiState;
+};
+
+export const MBTIStateToValue = (mbti: MBTIState[]) => {
+  let mbtiValue: Mbti = {
+    date: new Date().getUTCMilliseconds(),
+    EI: mbti[0].state == true ? 25 : 75,
+    NS: mbti[1].state == true ? 25 : 75,
+    FT: mbti[2].state == true ? 25 : 75,
+    JP: mbti[3].state == true ? 25 : 75,
+  };
+  return mbtiValue;
+};
